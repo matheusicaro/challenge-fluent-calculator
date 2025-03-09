3c2dac7ddbdc6eaa5be09d6d8e7b476e79185826
Matheus Icaro <matheusicaro2@hotmail.com>
2025-03-09 16:59:37 -0600

    Fixed database values for default env

--- test/models/calc_test.rb
+++ test/models/calc_test.rb
@@ -1,7 +1,9 @@
 require "test_helper"
 
 class CalcTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+  test "should do math plus correctly for each number" do
+    expected_
+    result = Calc.new.two.plus.one
+    assert_equal(2, result, "the value should be 3")
   end
 end
